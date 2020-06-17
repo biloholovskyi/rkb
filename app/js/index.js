3cbd64d08ab305904afdce5053f88047b04115c6
Dychkevych Kolya <dychkevych.kolya@gmail.com>
2020-06-17 16:40:56 +0300

    page for pacients/ fixes accordion

--- app/js/index.js
+++ app/js/index.js
@@ -9,6 +9,14 @@ $(document).ready((e) => {
   $('input,textarea').on('input', inputForm);
 
 
+  // aside bar add active
+  $('.aside ul li').on('click', function(){
+    if(!$(this).hasClass('active')){
+      $(this).siblings().removeClass('active');
+      $(this).addClass('active');
+      }
+  });
+
 });
 
 $(window).resize(() => {
